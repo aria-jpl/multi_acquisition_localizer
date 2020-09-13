70bfa419bfa34ad6513ee27feea97353901387ab
Mohammed Karim <widewidesea2017@gmail.com>
2020-09-12 18:43:32 -0700

    local

--- acquisition_localizer_single.py
+++ acquisition_localizer_single.py
@@ -349,7 +349,7 @@ def sling_extract_job(sling_extract_version, slc_id, url_type, download_url, que
     logger.info("\nsling_extract_job for :%s" %slc_id)
     job_type = "job-spyddder-sling-extract-{}:{}".format(url_type, sling_extract_version)
  
-    if destination_type.lower()=="file":
+    if destination_type.lower()=="file" or destination_type.lower()=="local":
         job_type = "job-spyddder-sling-extract-file-{}:{}".format(url_type, sling_extract_version)
 
     # resolve hysds job
